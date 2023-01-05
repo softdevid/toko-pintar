1371a4c11648646c91dccf01efa5c89e22a1d3c0
Ardiputra614 <ardianto8306@gmail.com>
2023-01-05 12:53:38 +0700

    /

---
+++ database/migrations/2023_01_05_054747_create_members_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('idUser');
+            $table->string('namaMember');
+            $table->string('kodeMember');
+            $table->string('email');
+            $table->string('password');
+            $table->string('noHp');
+            $table->string('alamat');
+            $table->date('tanggalLahir');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+}
