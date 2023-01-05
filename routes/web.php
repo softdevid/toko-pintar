9c5679aafc26a74cb2d552aa7f5e353b0462a212
Ardiputra614 <ardianto8306@gmail.com>
2023-01-05 15:03:43 +0700

    /

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\KaryawanController;
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\MemberController;
 use App\Http\Controllers\ProdukController;
+use App\Http\Controllers\TokoController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -21,43 +22,19 @@ use Inertia\Inertia;
 
 
 //route CRUD produk
-Route::get('/produk', [ProdukController::class, 'index'])->name('produk.index');
-Route::get('/produk/create', [ProdukController::class, 'create'])->name('produk.create');
-Route::post('/produk/store', [ProdukController::class, 'store'])->name('produk.store');
-Route::get('/produk/{id}/edit', [ProdukController::class, 'edit'])->name('produk.edit');
-Route::patch('/produk/update', [ProdukController::class, 'update'])->name('produk.update');
-Route::delete('/produk/delete/{id}', [ProdukController::class, 'destroy'])->name('produk.destroy');
+Route::resource('produk', ProdukController::class);
 
 //route CRUD Member
-Route::get('/member', [MemberController::class, 'index'])->name('member.index');
-Route::get('/member/create', [MemberController::class, 'create'])->name('produk.create');
-Route::post('/member/store', [MemberController::class, 'store'])->name('member.store');
-Route::get('/member/{id}/edit', [MemberController::class, 'edit'])->name('member.edit');
-Route::patch('/member/update', [MemberController::class, 'update'])->name('member.update');
-Route::delete('/member/delete/{id}', [MemberController::class, 'destroy'])->name('member.destroy');
+Route::resource('member', MemberController::class);
 
 //route CRUD Kategori
-Route::get('/kategori', [KategoriController::class, 'index'])->name('kategori.index');
-Route::post('/kategori/store', [KategoriController::class, 'store'])->name('kategori.store');
-Route::patch('/kategori/update', [KategoriController::class, 'update'])->name('kategori.update');
-Route::delete('/kategori/delete/{id}', [KategoriController::class, 'destroy'])->name('kategori.destroy');
+Route::resource('kategori', KategoriController::class);
 
 //route CRUD Karyawan
-Route::get('/karyawan', [KaryawanController::class, 'index'])->name('karyawan.index');
-Route::get('/karyawan/create', [KaryawanController::class, 'create'])->name('produk.create');
-Route::post('/karyawan/store', [KaryawanController::class, 'store'])->name('karyawan.store');
-Route::get('/karyawan/{id}/edit', [KaryawanController::class, 'edit'])->name('karyawan.edit');
-Route::patch('/karyawan/update', [KaryawanController::class, 'update'])->name('karyawan.update');
-Route::delete('/karyawan/delete/{id}', [KaryawanController::class, 'destroy'])->name('karyawan.destroy');
+Route::resource('karyawan', KaryawanController::class);
 
 //route CRUD Karyawan
-Route::get('/toko', [TokoController::class, 'index'])->name('toko.index');
-Route::get('/toko/create', [TokoController::class, 'create'])->name('produk.create');
-Route::post('/toko/store', [TokoController::class, 'store'])->name('toko.store');
-Route::get('/toko/{id}/edit', [TokoController::class, 'edit'])->name('toko.edit');
-Route::patch('/toko/update', [TokoController::class, 'update'])->name('toko.update');
-Route::delete('/toko/delete/{id}', [TokoController::class, 'destroy'])->name('toko.destroy');
-
+Route::resource('toko', TokoController::class);
 
 
 
