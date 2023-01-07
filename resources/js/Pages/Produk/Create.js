fff94b5653c47c44f131762c8df7416ab06b1669
zionexeonch <zionexeon@gmail.com>
2023-01-07 15:47:41 +0700

    next code

--- resources/js/Pages/Produk/Create.js
+++ resources/js/Pages/Produk/Create.js
@@ -60,6 +60,23 @@ const Create = () => {
           </div>
           <div>
             <label
+              htmlFor="satuan"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            >
+              Kategori
+            </label>
+            <select
+              id="satuan"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              name="kategori"
+            >
+              <option>Pilih Satuan</option>
+
+              <option value=""></option>
+            </select>
+          </div>
+          <div>
+            <label
               htmlFor="nama"
               className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
             >
