b9cc801b8ab29849749d10e970689b3685abed14
zionexeonch <zionexeon@gmail.com>
2023-01-11 12:06:14 +0700

    next committed

--- resources/js/Pages/Produk/Index.js
+++ resources/js/Pages/Produk/Index.js
@@ -1,5 +1,4 @@
 import Main from "@/Components/Admin/Main";
-import { Link } from "@inertiajs/inertia-react";
 import React from "react";
 
 const Index = () => {
@@ -7,32 +6,41 @@ const Index = () => {
     <>
       <div className="flex flex-row justify-between items-between my-4">
         <h1 className="font-bold text-2xl">Stok Produk</h1>
-        <Link
+        <a
           href={route("produk.create")}
           type="button"
           className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
         >
           Tambah Produk
-        </Link>
+        </a>
       </div>
       <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
         <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
           <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
               <th scope="col" className="px-6 py-3">
-                Product name
+                Nama Produk
               </th>
               <th scope="col" className="px-6 py-3">
-                Color
+                Kategori
               </th>
               <th scope="col" className="px-6 py-3">
-                Category
+                Satuan
               </th>
               <th scope="col" className="px-6 py-3">
-                Price
+                Harga Beli
               </th>
               <th scope="col" className="px-6 py-3">
-                <span className="sr-only">Edit</span>
+                Harga Jual
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Stok Gudang
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Stok Toko
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Action
               </th>
             </tr>
           </thead>
@@ -47,50 +55,16 @@ const Index = () => {
               <td className="px-6 py-4">Sliver</td>
               <td className="px-6 py-4">Laptop</td>
               <td className="px-6 py-4">$2999</td>
-              <td className="px-6 py-4 text-right">
+              <td className="px-6 py-4">$2999</td>
+              <td className="px-6 py-4">$2999</td>
+              <td className="px-6 py-4">$2999</td>
+              <td className="px-6 py-4">
                 <a
-                  href="#"
-                  className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                  href={route("produk.edit", [1])}
+                  type="button"
+                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
                 >
-                  Edit
-                </a>
-              </td>
-            </tr>
-            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
-                Microsoft Surface Pro
-              </th>
-              <td className="px-6 py-4">White</td>
-              <td className="px-6 py-4">Laptop PC</td>
-              <td className="px-6 py-4">$1999</td>
-              <td className="px-6 py-4 text-right">
-                <a
-                  href="#"
-                  className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                >
-                  Edit
-                </a>
-              </td>
-            </tr>
-            <tr className="bg-white dark:bg-gray-800 hover:bg-gray-50 dark:hover:bg-gray-600">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
-                Magic Mouse 2
-              </th>
-              <td className="px-6 py-4">Black</td>
-              <td className="px-6 py-4">Accessories</td>
-              <td className="px-6 py-4">$99</td>
-              <td className="px-6 py-4 text-right">
-                <a
-                  href="#"
-                  className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                >
-                  Edit
+                  Ubah
                 </a>
               </td>
             </tr>
