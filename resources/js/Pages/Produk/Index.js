a773a525a7418dd3aba7830668cc97d8aca0a1b3
zionexeonch <zionexeon@gmail.com>
2023-01-18 10:01:32 +0700

    next commit

--- resources/js/Pages/Produk/Index.js
+++ resources/js/Pages/Produk/Index.js
@@ -22,13 +22,13 @@ const Index = (props) => {
     <>
       <div className="flex flex-row justify-between items-between my-4">
         <h1 className="font-bold text-2xl">Stok Produk</h1>
-        <a
+        <Link
           href={route("produk.create")}
           type="button"
           className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
         >
           Tambah Produk
-        </a>
+        </Link>
       </div>
       <div className="mt-1 mb-7 mx-auto max-w-xl">
         <div className="flex items-center">
@@ -112,13 +112,20 @@ const Index = (props) => {
                     <td className="px-6 py-4">{data.stokGudang}</td>
                     <td className="px-6 py-4">{data.stokToko}</td>
                     <td className="px-6 py-4 text-right">
-                      <a
+                      <Link
                         href={route("produk.edit", [1])}
                         type="button"
                         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
                       >
                         Ubah
-                      </a>
+                      </Link>
+                      <Link
+                        href={route("produk.destroy", [1])}
+                        type="button"
+                        className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+                      >
+                        Hapus
+                      </Link>
                     </td>
                   </tr>
                 );
